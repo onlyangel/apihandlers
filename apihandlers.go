feb06ca858deb7fc05b8dfa598b30369cd06fbf6
Jose Angel Espinoza Portillo <jose@ngel.es>
2018-08-30 22:27:14 -0500

    changes for api

--- apihandlers.go
+++ apihandlers.go
@@ -3,8 +3,11 @@ package apihandlers
 import (
 	"net/http"
 	"fmt"
-	"appengine"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"time"
 	"github.com/go-errors/errors"
+	"encoding/json"
 )
 
 type infunc func(http.ResponseWriter, *http.Request)
@@ -18,7 +21,31 @@ func Recover( fn infunc ) infunc{
 			if err := recover(); err != nil {
 				err := errors.Wrap(err, 1)
 				fmt.Fprintf(w,"ERROR: %v", err)
-				ctx.Infof("ERROR: %v", err.ErrorStack())
+				log.Criticalf(ctx,"ERROR: %v", err.ErrorStack())
+			}
+		}(r)
+
+		fn(w,r)
+	}
+}
+
+func RecoverApi( fn infunc ) infunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		defer func(r *http.Request) {
+			ctx := appengine.NewContext(r)
+			if err := recover(); err != nil {
+				err := errors.Wrap(err, 1)
+				mp := struct{
+					Error string
+				}{
+					Error: err.Error(),
+				}
+				jsonstr , _ := json.Marshal(mp)
+				fmt.Fprintf(w,"%s", string(jsonstr))
+				ms := time.Now().UnixNano()
+				log.Infof(ctx,"ERROR(%d): ------------ ", ms)
+				log.Infof(ctx,"ERROR(%d): %s", ms, err.Error())
+				log.Criticalf(ctx,"ERROR(%d): %v", ms, err.ErrorStack())
 			}
 		}(r)
 
