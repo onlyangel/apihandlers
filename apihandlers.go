b1149882e8c76c386b3281410a86192de35d42eb
onlyangel <jose@ngel.es>
2020-11-10 04:59:24 -0600

    replace fatal with log

--- apihandlers.go
+++ apihandlers.go
@@ -10,25 +10,26 @@ import (
 )
 
 type infunc func(http.ResponseWriter, *http.Request)
-func Recover( fn infunc ) infunc{
-	return func(w http.ResponseWriter, r *http.Request){
+
+func Recover(fn infunc) infunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		defer func(r *http.Request) {
 			if err := recover(); err != nil {
 				err := errors.Wrap(err, 1)
-				fmt.Fprintf(w,"ERROR: %v", err)
+				fmt.Fprintf(w, "ERROR: %v", err)
 				log.Fatalf("ERROR: %v", err.ErrorStack())
 			}
 		}(r)
 
-		fn(w,r)
+		fn(w, r)
 	}
 }
 
-func RecoverApi( fn infunc ) infunc {
-	return func(w http.ResponseWriter, r *http.Request){
+func RecoverApi(fn infunc) infunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		defer func(r *http.Request) {
 			if errr := recover(); errr != nil {
 
@@ -36,37 +37,37 @@ func RecoverApi( fn infunc ) infunc {
 				mp := ErrorType{
 					Error: err.Error(),
 				}
-				jsonstr , _ := json.Marshal(mp)
-				fmt.Fprintf(w,"%s", string(jsonstr))
+				jsonstr, _ := json.Marshal(mp)
+				fmt.Fprintf(w, "%s", string(jsonstr))
 				ms := time.Now().UnixNano()
 				log.Printf("ERROR(%d): ------------ ", ms)
 				log.Printf("ERROR(%d): %s", ms, err.Error())
-				log.Fatalf("ERROR(%d): %v", ms, err.ErrorStack())
+				log.Printf("ERROR(%d): %v", ms, err.ErrorStack())
 			}
 		}(r)
 
-		fn(w,r)
+		fn(w, r)
 	}
 }
 
-func PanicIfNotNil(err error){
+func PanicIfNotNil(err error) {
 	if err != nil {
 		panic(errors.Wrap(err, 1))
 	}
 }
 
-func PanicWithMsg( str string){
-	panic(fmt.Errorf("%s",str))
+func PanicWithMsg(str string) {
+	panic(fmt.Errorf("%s", str))
 }
 
-type ErrorType struct{
+type ErrorType struct {
 	Error string
 }
 
-func WriteAsJson(w http.ResponseWriter, obj interface{}){
+func WriteAsJson(w http.ResponseWriter, obj interface{}) {
 	PanicIfNotNil(json.NewEncoder(w).Encode(obj))
 }
-func WriteAsJsonList(w http.ResponseWriter, obj interface{}){
+func WriteAsJsonList(w http.ResponseWriter, obj interface{}) {
 
 	jsonstr, err := json.Marshal(obj)
 	PanicIfNotNil(err)
